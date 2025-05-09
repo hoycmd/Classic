0d8694e526a5d0d68f6c9ea62cfbffd403376a66
ƬN丅 ｌlᴠɆ (ᴼᶠᶠⁱᶜⁱᵃˡ) <145314654+hoycmd@users.noreply.github.com>
2025-05-10 01:12:37 +0700

    Update gamemode.js

--- client/room/gamemode.js
+++ client/room/gamemode.js
@@ -3,8 +3,10 @@ import * as room from 'pixel_combats/room';
 import * as basic from 'pixel_combats/basic';
 
 // * Создаём команды. * //
-let BlueTeam = CreateNewTeam('Blue', 'Teams/Blue \n Команда синия', new basic.Color(0, 0, 1, 0), room.BuildBlocksSet.Blue, room.Spawns.SpawnPointsGroups.Add(1));
-let RedTeam = CreateNewTeam('Red', 'Teams/Red \n Команда красная', new basic.Color(1, 0, 0, 0), room.BuildBlocksSet.Red, room.Spawns.SpawnPointsGroups.Add(2));
+let BlueTeam = CreateNewTeam('Blue', 'Teams/Blue \n Команда синия', new basic.Color(0, 0, 1, 0), room.BuildBlocksSet.Blue);
+room.Spawns.SpawnPointsGroups.Add(1);
+let RedTeam = CreateNewTeam('Red', 'Teams/Red \n Команда красная', new basic.Color(1, 0, 0, 0), room.BuildBlocksSet.Red);
+room.Spawns.SpawnPointsGroups.Add(2);
 // * Настройки, интерфейс - команд. * //
 room.Ui.GetContext().TeamProp1.Value = { Team: 'Blue', Prop: SCORES_PROP_NAME };
 room.Ui.GetContext().TeamProp2.Value = { Team: 'Red', Prop: SCORES_PROP_NAME };
@@ -114,6 +116,135 @@ win_team = null;
 		StartEndOfGame(null);
 	    }
 }
+function OnGameStateTimer() // Конец, матча.
+{
+	TrySwitchGameState();
+}
+room.Damage.OnDeath.Add(TrySwitchGameState);
+room.Players.OnPlayerDisconnected.Add(TrySwitchGameState);
+
+// * Настройки, переключателей - режима. * //
+MainTimer.OnTimer.Add(function() {
+ switch (StateProp.value) {
+	case WaitingStateValue:
+		SetBuildMode();
+		break;
+	case BuildModeStateValue:
+		SetGameMode();
+		break;
+	case GameStateValue:
+		OnGameStateTimer();
+		break;
+	case EndOfGameStateValue:
+		EndEndOfGame();
+		break;
+	case EndOfMatchStateValue:
+		RestartGame();
+		break;
+	}
+});
+
+// * Задаём, первое - игровое состояние. * //
+SetWaitingMode();
+
+// * Состояние игры. * //
+function SetWaitingMode() { 
+ StateProp.value = WaitingStateValue;
+ room.Ui.GetContext().Hint.Value = 'Ожидание, всех - игроков...';
+ room.Spawns.GetContext().Enable = false;
+ room.TeamsBalancer.IsAutoBalance = true;
+ MainTimer.Restart(WaitingModeSeconts);
+}
+
+function SetBuildMode() 
+{
+ StateProp.value = BuildModeStateValue;
+ room.Ui.GetContext().Hint.Value = 'Застраивайте базу, и разрушайте - базу врагов!';
+
+ const inventory = room.Inventory.GetContext();
+  inventory.Main.Value = false;
+  inventory.Secondary.Value = false;
+  inventory.Melee.Value = true;
+  inventory.Explosive.Value = false;
+  inventory.Build.Value = true;
+
+ MainTimer.Restart(BuildModeSeconds);
+ room.Spawns.GetContext().enable = true;
+ room.TeamsBalancer.IsAutoBalance = true; 
+ SpawnTeams();
+}
+function SetGameMode() 
+{
+	StateProp.value = GameStateValue;
+	room.Ui.GetContext().Hint.Value = 'Атакуйте, всех - врагов!';
+	win_team_id_prop.Value = null; 
+
+	var inventory = room.Inventory.GetContext();
+	if (room.GameMode.Parameters.GetBool('OnlyKnives')) {
+	  inventory.Main.Value = false;
+	  inventory.Secondary.Value = false;
+	  inventory.Melee.Value = true;
+	  inventory.Explosive.Value = false;
+	  inventory.Build.Value = true;
+	} else {
+	  inventory.Main.Value = true;
+	  inventory.Secondary.Value = true;
+	  inventory.Melee.Value = true;
+          inventory.Explosive.Value = true;
+	  inventory.Build.Value = true;
+	}
+
+	MainTimer.Restart(GameModeSeconds);
+	room.Spawns.GetContext().Despawn();
+	room.Spawns.GetContext().RespawnEnable = false;
+	TeamsBalancer.IsAutoBalance = false;
+	TeamsBalancer.BalanceTeams();
+	SpawnTeams();
+}
+function StartEndOfGame(t) { 
+log.debug("win team="+team);
+ StateProp.value = EndOfGameStateValue;
+ if (t !== null) {
+log.debug(1);
+	room.Ui.GetContext().Hint.Value = team + ' победила!';
+		const prop = t.Properties.Get(SCORES_PROP_NAME);
+		 if (prop.Value == null) prop.Value = 1;
+		  else prop.Value = prop.Value + 1;
+	}
+	else Ui.GetContext().Hint.Value = 'Hint/Draw';
+     MainTimer.Restart(EndGameSeconds);
+}
+function EndEndOfGame(){
+	if (win_team_id_prop.Value !== null) {
+	const prop = room.Teams.Get(win_team_id_prop.Value);
+	  const prop = t.Properties.Get(scoresProp);
+		if (prop.Value >= MaxScores) SetEndOfMatchMode();
+		else SetGameMode();
+	}
+	else SetGameMode();
+}
+
+function SetEndOfMatchMode() {
+	StateProp.value = EndOfMatchStateValue;
+	room.Ui.GetContext().Hint.Value = 'Конец, матча!';
+
+	var context = room.Spawns.GetContext();
+	context.Enable = false;
+	context.Despawn();
+	room.Game.GameOver(room.LeaderBoard.GetTeams());
+	MainTimer.Restart(EndOfMatchTime);
+}
+function RestartGame() {
+	room.Game.RestartGame();
+}
+
+function SpawnTeams() {
+  for (const Team of room.Teams) {
+   Spawns.GetContext(Team).Spawn();
+	}
+}
+
+
 
 
 
